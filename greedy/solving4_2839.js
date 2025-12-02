8c970b863a4396a2df549399bec39dc52a567b11
HarryKim24 <tl9434@naver.com>
2025-12-02 11:16:32 +0900

    add solving4_2839.js

---
+++ greedy/solving4_2839.js
@@ -0,0 +1,28 @@
+// 설탕 배달 2839(실버4)
+
+/*
+  값이 5로 나누어 떨어지는 경우, 5로 나누기
+  값이 5로 나누어 떨어지지 않으면 5로 나누어 떨어질 때까지 3을 빼기
+*/
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let n = Number(input[0]);
+let cnt = 0;
+let flag = false;
+
+while(n) {
+  if (n == 0 || n % 5 == 0) {
+    cnt += Math.floor(n / 5);
+    console.log(cnt);
+    flag = true;
+    break;
+  }
+  n -= 3;
+  cnt++;
+}
+
+if (!flag) {
+  console.log(-1);
+}
