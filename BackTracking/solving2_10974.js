c28b494b26635c3a297cecdb0fcbfddfd5097e19
HarryKim24 <tl9434@naver.com>
2025-12-04 11:14:33 +0900

    add solving2_10974

---
+++ BackTracking/solving2_10974.js
@@ -0,0 +1,36 @@
+// 모든 순열 10974(실버3)
+
+/*
+  백트래킹
+*/
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let n = Number(input[0]);
+
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(i);
+}
+let visited = Array(n).fill(false);
+let selected = [];
+let result = '';
+
+function bfs(arr, depth) {
+  if (depth == n) {
+    result += selected.join(' ') + '\n';
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i]) continue;
+    selected.push(arr[i]);
+    visited[i] = true;
+    bfs(arr, depth + 1);
+    selected.pop();
+    visited[i] = false;
+  }
+}
+bfs(arr, 0);
+console.log(result);
